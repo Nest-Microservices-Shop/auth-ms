b31c3b300edf50f86300865a9efca9c046133eba
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-06-18 13:21:51 -0400

    feat: auth-ms done

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,121 @@
+import {
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { ClientProxy, Payload, RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { envs } from 'src/config';
+
+@Injectable()
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('Authservice');
+
+  constructor(private readonly jwtService: JwtService) {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+
+  async signJWT(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { email, name, password } = registerUserDto;
+
+    try {
+      const user = await this.user.findUnique({ where: { email } });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exist',
+        });
+      }
+
+      const newUser = await this.user.create({
+        data: {
+          email: email,
+          name: name,
+          password: bcrypt.hashSync(password, 10),
+        },
+      });
+
+      const { password: __, ...rest } = newUser;
+
+      return {
+        user: rest,
+        token: await this.signJWT(rest),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    try {
+      const user = await this.user.findUnique({ where: { email } });
+
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User not found',
+        });
+      }
+
+      const isValidPassword = bcrypt.compareSync(password, user.password);
+
+      if (!isValidPassword) {
+        throw new RpcException({
+          status: HttpStatus.FOUND,
+          message: 'Invalid password',
+        });
+      }
+
+      const { password: __, ...rest } = user;
+
+      return {
+        user: rest,
+        token: await this.signJWT(rest),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async verifyToken(token: string) {
+    try {
+      const { sub, iat, exp, ...user } = this.jwtService.verify(token, {
+        secret: envs.jwtSecret,
+      });
+
+      return {
+        user: user,
+        token: await this.signJWT(user),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.FORBIDDEN,
+        message: 'Token not valid',
+      });
+    }
+  }
+}
